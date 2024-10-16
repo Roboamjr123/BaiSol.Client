47623723406167a1d0bc7740d4ba7347e419dbba
Richard Quirante <148254683+Ritsard404@users.noreply.github.com>
2024-10-16 17:39:33 +0800

    Delete src/Admin/pages/ActivityLogPage.tsx

--- src/Admin/pages/ActivityLogPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import ActivitityLogs from "../components/logs/ActivitityLogs";
-
-const ActivityLogPage = () => {
-  // return <ActivitityLogs />;
-  return <></>
-};
-
-export default ActivityLogPage;
